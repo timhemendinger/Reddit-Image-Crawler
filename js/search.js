7b6a62c8ff70cd9e964f5f91a2aab0504686bc71
Tim Hemendinger <timhemendinger@gmail.com>
2015-05-25 20:53:49 -0400

    Thumbnails are displayed rather than full-sized images, adding a little bit of styling, link to full-sized image.

--- js/search.js
+++ js/search.js
@@ -1,17 +1,31 @@
 
 $('search_btn').addEventListener('click', function() {
-	var subreddit = 'http://www.reddit.com/r/'+($('sub_text').value)+'/.json?';
+
+	var sub_text = $('sub_text');
+
+	// Clear the images displayed (if there was a previous search)
+	$('images').innerHTML = '';
+
+	// Reddit JSON URL for Subreddit entered
+	var subreddit = 'http://www.reddit.com/r/'+(sub_text.value)+'/.json?';
 
 	var images = [];
 
 	getJSON(subreddit, function(data){
-
+		console.log(data);
+		// Iterate through the URLS
 		for(var x=0; x<data.data.children.length; x++) {
-	 		addImage(data.data.children[x].data.url, 'images')
+	 		addImage(data.data.children[x].data.thumbnail,
+	 		data.data.children[x].data.url, 'images')
 		}
 
 	});
 
+}, false);
+
+// Clear search text box when clicked
+$('sub_text').addEventListener('click', function(){
+	this.value = '';
 }, false);
 
 function getJSON(path, callback) {
@@ -29,12 +43,19 @@ function getJSON(path, callback) {
     httpRequest.send(); 
 };
 
-function addImage(url, location) {
+// Add image to location in DOM
+function addImage(thumbURL, url, location) {
 	var newIMG = document.createElement('IMG');
-	newIMG.src = url;
-	$(location).appendChild(newIMG);
+	var newLink = document.createElement('A');
+	newLink.href = url;
+	newIMG.src = thumbURL;
+
+	newLink.appendChild(newIMG);
+
+	$(location).appendChild(newLink);
 }
 
+// Helper function, for brevity
 function $(item) {
 	return document.getElementById(item);
 }
